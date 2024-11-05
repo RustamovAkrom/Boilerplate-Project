a3007e14bb39dca5bb4ee4602b88c8a7be422bc3
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-11-05 13:56:34 +0500

    create processing models

--- apps/processing/models.py
+++ apps/processing/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from apps.shared.models import TimeStampedModel
+
+
+class UzcardHistroyRequest(TimeStampedModel):
+    cart_id = models.CharField(max_length=128)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    page = models.IntegerField()
+    page_size = models.IntegerField()
+
+    class Meta:
+        verbose_name = "UzCardHistory"
+        verbose_name_plural = "UzCardHistories"
+
+
+class HumoCardHistoryRequest(TimeStampedModel):
+    card_number = models.CharField(max_length=16)
+    start_date = models.DateField(max_length=16)
+    end_date = models.DateField(max_length=16)
+
+    class Meta:
+        verbose_name = "HumoCardHistory"
+        verbose_name_plural = "HumoCardHistories"
+
+
+class MasterCardHistoryRequest(TimeStampedModel):
+    card_number = models.CharField(max_length=16)
+    start_date = models.DateField(max_length=16)
+    end_date = models.DateField(max_length=16)
+
+    class Meta:
+        verbose_name = "MasterCardHistory"
+        verbose_name_plural = "MasterCardHistories"
