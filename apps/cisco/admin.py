878adf6f01f27a90bc69403651dbad2d8f659e22
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-11-05 15:58:48 +0500

    register ciso model in admin panel

--- apps/cisco/admin.py
+++ apps/cisco/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ResourceStats
+
+
+@admin.register(ResourceStats)
+class ResourcesStatsAdmin(admin.ModelAdmin):
+    list_filter = ("resourceName", )
