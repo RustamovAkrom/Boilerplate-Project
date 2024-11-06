f60adbb08caf1e3cf9493fe8f9b7a0514ff66c5c
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-11-06 12:39:52 +0500

    register telemarketing models in admin panel

--- apps/telemarketing/admin.py
+++ apps/telemarketing/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Credit,
+    Type,
+    CustomerCategory,
+    Customer,
+    Appeal,
+    AppealStatus,
+    Comment,
+    Chouse,
+    Product,
+    Channel,
+    ScriptCategory,
+    Script,
+    OptionObject,
+    Option,
+    CallScript,
+    Branch,
+    Process
+)
+
+@admin.register(Credit)
+class CreditAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CustomerCategory)
+class CustomerCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AppealStatus)
+class AppealStatusAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "parent_id", )
+
+    def parent_id(self, obj):
+        return obj.parent.id  if obj.parent else None
+    
+    parent_id.short_description = "Parent Id"
+
+
+@admin.register(ScriptCategory)
+class ScriptCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Script)
+class ScriptAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CallScript)
+class CallScriptAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(
+    [Customer, Appeal, Channel, Chouse, Comment, OptionObject, Branch, Process]
+)
